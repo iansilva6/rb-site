376c19a86b107e254790084f6f270fc9303cb3aa
Ian Silva <iansilva6@yahoo.com.br>
2021-08-29 15:07:15 -0300

    fix: improve lh note

--- src/components/WhatsappButton/index.js
+++ src/components/WhatsappButton/index.js
@@ -8,7 +8,7 @@ import { Float } from './styles';
 
 function WhatsappButton(props) {
     return (
-        <Float target={"_blank"} href={"https://api.whatsapp.com/send?phone=5571991985448&text=Ol%C3%A1%2C%20vim%20atrav%C3%A9s%20do%20site%20www.rbinspecoes.com.br.%20"}>
+        <Float target={"_blank"} href={"https://api.whatsapp.com/send?phone=5571991985448&text=Ol%C3%A1%2C%20vim%20atrav%C3%A9s%20do%20site%20www.rbinspecoes.com.br.%20"} rel="noopener" aria-label="Whatsapp">
             <FontAwesomeIcon icon={faMobileAlt} size="1x" />
         </Float>
     );
