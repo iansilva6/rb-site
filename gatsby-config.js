44cf7db1552247ac9e10ed680df868aeff795593
Ian Silva <iansilva6@yahoo.com.br>
2021-08-29 12:57:42 -0300

    feat: improve images

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,9 @@ module.exports = {
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, 
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { StaticImage } from "gatsby-plugin-image";
 
 // Styles
 import { Container, Text } from './styles';
 
-// Images
-import Logo from '../../images/logo.webp';
-
 function Banner(props) {
     return (
         <Container>
-            <img width={135} height={135} src={Logo} alt="RB Consultoria, Inspeção e Engenharia. Especializado em NR-13"></img>
+            <StaticImage 
+                width={135} 
+                height={135} 
+                src="../../images/logo.webp" 
+                alt="RB Consultoria, Inspeção e Engenharia. Especializado em NR-13" 
+                placeholder="blurred"
+                layout="fixed"
+            />
             <Text>
                 <h1>RB Inspeções, Consultoria e Engenharia.</h1>
                 <p>NR-13 | Ensaios Não Destrutivos | Engenharia | Manutenção Alpinismo Industrial</p>
